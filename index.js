c5389d2daacd0332a963d46ce7e13646c1da7b6c
Cedrick Campoto <cedrick.campoto@yahoo.com>
2023-10-03 21:51:17 +0800

    update

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ app.use("/",(req,res,next)=>{
     console.log('host ',req.header('Host'));
 });
 
-app.listen(80,(err)=>{
-    if(err){
-        return console.log('app failed to start ',err);
-    };
-    console.log('listening to port ',80);
-});
+
+require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+        configDir: "./greenlock.d",
+ 
+        // contact for security and critical bug notices
+        maintainerEmail: "jon@example.com",
+ 
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
+
+// app.listen(80,(err)=>{
+//     if(err){
+//         return console.log('app failed to start ',err);
+//     };
+//     console.log('listening to port ',80);
+// });
